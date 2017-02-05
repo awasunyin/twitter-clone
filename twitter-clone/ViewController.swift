79483ccf91d128210b1b6785e59c13e062a8bb10
awasunyin <awasunyin@gmail.com>
2017-02-05 21:03:31 +0100

    first commit

---
+++ twitter-clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter-clone
+//
+//  Created by Haohua Sun Yin on 5/2/17.
+//  Copyright © 2017 Awa Sun Yin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
