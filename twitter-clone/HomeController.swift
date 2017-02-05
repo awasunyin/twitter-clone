9b712b5bc8a4d68f925f18a54f6e486c4a4aa9ff
awasunyin <awasunyin@gmail.com>
2017-02-05 21:26:34 +0100

    added collection view cells and header

---
+++ twitter-clone/HomeController.swift
@@ -0,0 +1,87 @@
+//
+//  HomeController.swift
+//  twitter-clone
+//
+//  Created by Haohua Sun Yin on 5/2/17.
+//  Copyright © 2017 Awa Sun Yin. All rights reserved.
+//
+
+import UIKit
+
+class WordCell: UICollectionViewCell {
+    
+    //this gets called when a cell is dequed
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //setting the background color of the cells
+        setupViews()
+    }
+    
+    //construct label progratically
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "test"
+        //needs this, otherwise cells won't show up
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        backgroundColor = .yellow
+        
+        //adds the element to the container
+        addSubview(wordLabel)
+        
+        //setting up constraints to the elements
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    let headerId = "headerId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        //registered the header
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+    }
+    
+    //sets number of cells in the collection view
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    //setting the size of the cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        return header
+    }
+    
+    //setting the header size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+}
