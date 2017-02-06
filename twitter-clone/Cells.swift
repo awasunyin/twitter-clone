7c56f10cb3551424d5711ebdb1721b6076c2933b
awasunyin <awasunyin@gmail.com>
2017-02-06 22:22:04 +0100

    fixed view elements

--- twitter-clone/Cells.swift
+++ twitter-clone/Cells.swift
@@ -8,18 +8,43 @@
 
 import LBTAComponents
 
+//accessible for all classes
+let twitterBlue = UIColor(r: 61, g: 176, b: 244)
+
+
 class UserHeader: DatasourceCell {
+    
+    let textLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Who to follow"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+        
+    }()
     
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .blue
+        
+        addSubview(textLabel)
+        textLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
     }
 }
 
 class UserFooter: DatasourceCell {
+    
+    let textLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Show me more"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+        
+    }()
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .green
+        
+        addSubview(textLabel)
+        
+        textLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
     }
 }
 
@@ -68,7 +93,6 @@ class UserCell: DatasourceCell {
     
     let followButton: UIButton = {
         
-        let twitterBlue = UIColor(r: 61, g: 176, b: 244)
 
         let button = UIButton()
         button.layer.cornerRadius = 5
